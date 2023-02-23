18064bb6a17367f369c50f254f26a58fb1904a58
chbarbosa <carloshenriquefb@gmail.com>
2023-02-23 16:23:15 -0500

    listing posts added

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,42 +6,32 @@ import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
 function PostsList({isModalVisible, onStopPosting}) {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
+    const [posts, setPosts] = useState([]);
 
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value)
+    function addPostHandler(postData){
+        setPosts((existingPosts) => [postData, ...existingPosts]);
     }
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value)
-    }
-
-    /*
-    
-    let modalContent;
-
-    if (modalIsVisible) {
-        modalContent = (
-            <Modal onClose={hideModalHandler}>            
-                <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
-            </Modal> );
-    }
-    */
 
     return (
         <>
             {isModalVisible && (
             <Modal onClose={onStopPosting}>            
-                <NewPost 
-                    onBodyChange={bodyChangeHandler} 
-                    onAuthorChange={authorChangeHandler}
-                    onCancel={onStopPosting}
+                <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}
                  />
             </Modal> )}
-            <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
-                <Post author="Manuel" body="Voir le cours complet!" />
-            </ul>
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => 
+                        <Post key={post.body} author={post.author} body={post.body} />
+                    )}
+                </ul>
+            )}
+            {posts.length === 0 && (
+                <div style={{ textAlign: 'center', color: 'white'}}>
+                    <h2>There are no posts yet.</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
         </>
     );
 }
