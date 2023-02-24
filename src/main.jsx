77d026868246c9db4e03c8cb596167b9415c995f
chbarbosa <carloshenriquefb@gmail.com>
2023-02-24 18:10:27 -0500

    loaders, action n PostDetails added

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import Posts from './routes/Posts'
-import NewPost from './routes/NewPost'
+import Posts, {loader as postsLoader} from './routes/Posts'
+import PostDetails, {postDetailsLoader} from './routes/PostDetails'
+import NewPost, { action as newPostAction } from './routes/NewPost'
 import RootLayout from './routes/RootLayout'
 import './index.css'
 
 const router = createBrowserRouter([
   {path: '/', element: <RootLayout />,
     children: [
-      {path: '/', element: <Posts />, children: [
-        {path: '/create-post', element: <NewPost />},
+      {
+        path: '/', 
+        element: <Posts />, 
+        loader: postsLoader,
+        children: [
+        {path: '/create-post', element: <NewPost />, action: newPostAction },
+        {path: '/:id', element: <PostDetails />, loader: postDetailsLoader,}
       ]},
     ],
   },
