9e2fb4172be0d6eb29de4dcf96b41b2ab0430112
chbarbosa <carloshenriquefb@gmail.com>
2023-03-02 10:56:01 -0500

    fix at Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ function Modal({children}) {
     return (
     <>
         <div className={classes.backdrop} onClick={closeHandler}>
-            <dialog open className={classes.modal}>{children}</dialog>
+            <dialog open className={classes.modal}  onClick={e => e.stopPropagation()}>{children}</dialog>
         </div>
     </>
     );
